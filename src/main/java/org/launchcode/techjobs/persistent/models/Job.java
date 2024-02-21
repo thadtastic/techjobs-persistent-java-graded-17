92fd17486f2413ada932f841e945dc2f5a4c20e3
thadtastic <leothaddeus92@gmail.com>
2024-02-21 17:49:01 -0600

    Changed employer field in Job class to an object, uncommented task three test

--- src/main/java/org/launchcode/techjobs/persistent/models/Job.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Job.java
@@ -2,16 +2,14 @@ package org.launchcode.techjobs.persistent.models;
 
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 
-import java.util.List;
 
 @Entity
 public class Job extends AbstractEntity {
 
-
-    private String employer;
+    @ManyToOne
+    private Employer employer;
     private String skills;
 
 
@@ -19,7 +17,7 @@ public class Job extends AbstractEntity {
     }
 
     // Initialize the id and value fields.
-    public Job(String anEmployer, String someSkills) {
+    public Job(Employer anEmployer, String someSkills) {
         super();
         this.employer = anEmployer;
         this.skills = someSkills;
@@ -28,11 +26,11 @@ public class Job extends AbstractEntity {
     // Getters and setters.
     
 
-    public String getEmployer() {
+    public Employer getEmployer() {
         return employer;
     }
 
-    public void setEmployer(String employer) {
+    public void setEmployer(Employer employer) {
         this.employer = employer;
     }
 
